85685b84c849414a8ae851f2a788ed4971deac60
samlawski <603320+samlawski@users.noreply.github.com>
2021-03-24 16:08:14 +0100

    ✨ Add StartTime input field

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import {hot} from "react-hot-loader"
 
 import "./App.css"
 import CurrentTime from "./components/CurrentTime"
+import StartTimeInput from "./components/StartTimeInput"
 
 const App = props => (
   <div className="App">
     <CurrentTime />
+
+    <StartTimeInput />
+
+    {/* <ItemRow>
+      <ItemReorderHandle />
+      <ItemTime />
+      <ItemDurationInput />
+      <ItemDescriptionInput />
+      <ItemDeleteBtn />
+    </ItemRow> */}
   </div>
 )
 
---
+++ src/components/StartTimeInput.js
@@ -0,0 +1,23 @@
+import React, {
+  useState,
+} from "react"
+
+export default props => {
+  const [time, setTime] = useState()
+
+  const regex24h = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/
+  const errorTimeInvalid = regex24h.test(time) ? null : `Please use the 24-hour time format HH:MM`
+
+  const handleInputChange = event => {
+    setTime(event.target.value)
+  }
+  
+  return (
+    <>
+      <label htmlFor="startTimeInput">Start time: </label>
+      <input onChange={handleInputChange} id="startTimeInput" type="time" placeholder="HH:MM"></input>
+      <br />
+      <small style={{color: "lightgrey" }}>{errorTimeInvalid}</small>
+    </>
+  )
+}
