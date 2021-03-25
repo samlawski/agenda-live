32cbc1c77783ab12fff47d9dc9d47f8359fe3eea
samlawski <603320+samlawski@users.noreply.github.com>
2021-03-25 10:41:29 +0100

    ♻️ refactor using state in the parent component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from "react"
+import React, {
+  useState
+} from "react"
 import {hot} from "react-hot-loader"
 
 import "./App.css"
 import CurrentTime from "./components/CurrentTime"
 import StartTimeInput from "./components/StartTimeInput"
 
-const App = props => (
-  <div className="App">
-    <CurrentTime />
+const App = props => {
+  const [state, setState] = useState(() => ({
+    startTime: new Date().toTimeString().substring(0,5),
+    items: [
+      {
+        durationInMinutes: 5,
+        description: "Welcome"
+      }
+    ]
+  }))
 
-    <StartTimeInput />
+  const handleStartTimeInputChange = event => setState(state => ({...state, startTime: event.target.value }))
 
-    {/* <ItemRow>
-      <ItemReorderHandle />
-      <ItemTime />
-      <ItemDurationInput />
-      <ItemDescriptionInput />
-      <ItemDeleteBtn />
-    </ItemRow> */}
-  </div>
-)
+  return (
+    <div className="App">
+      <CurrentTime />
+  
+      <StartTimeInput
+        startTime={state.startTime}
+        onChange={handleStartTimeInputChange} 
+      />
+
+    </div>
+  )
+}
 
 export default hot(module)(App)
+
+/* <ItemRow>
+  <ItemReorderHandle />
+  <ItemTime />
+  <ItemDurationInput />
+  <ItemDescriptionInput />
+  <ItemDeleteBtn />
+</ItemRow> */
--- src/components/StartTimeInput.js
+++ src/components/StartTimeInput.js
@@ -1,21 +1,19 @@
-import React, {
-  useState,
-} from "react"
+import React from "react"
 
 export default props => {
-  const [time, setTime] = useState(() => new Date().toTimeString().substring(0,5))
-
   const regex24h = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/
-  const errorTimeInvalid = !time || regex24h.test(time) ? null : `Please use the 24-hour time format HH:MM`
-
-  const handleInputChange = event => {
-    setTime(event.target.value)
-  }
+  const errorTimeInvalid = !props.startTime || regex24h.test(props.startTime) ? null : `Please use the 24-hour time format HH:MM`
   
   return (
     <>
       <label htmlFor="startTimeInput">Start time: </label>
-      <input onChange={handleInputChange} value={time} id="startTimeInput" type="time" placeholder="HH:MM"></input>
+      <input 
+        id="startTimeInput" 
+        type="time" 
+        placeholder="HH:MM"
+        onChange={props.onChange}
+        value={props.startTime}
+      />
       <br />
       <small style={{color: "lightgrey" }}>{errorTimeInvalid}</small>
     </>
