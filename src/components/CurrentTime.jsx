41d22873f3bb39f7b7ed86b52489527e28ec5302
samlawski <603320+samlawski@users.noreply.github.com>
2021-03-26 12:23:15 +0100

    ✨ display clock emoji based on current time

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -3,9 +3,14 @@ import React from "react"
 import "./CurrentTime.css"
 
 export default props => {
+  const currentHour = parseInt(props.currentTime.toLocaleTimeString([], { hour: "2-digit", hour12: true}).split(" ")[0])
+  const clockEmoji = [
+    "🕛", "🕐", "🕑", "🕒", "🕓", "🕔", "🕕", "🕖", "🕗", "🕘", "🕙", "🕚", "🕛"
+  ][currentHour]
+
   return (
     <div className="CurrentTime">
-      <div className="CurrentTime__inner">{props.currentTime.toLocaleTimeString()}</div>
+      <div className="CurrentTime__inner">{clockEmoji} {props.currentTime.toLocaleTimeString()}</div>
     </div>
   )
 }
